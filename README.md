83c846591c06e4ce9c469dc8f56296609ad2b308
Meet Patel <meet@meetpatel.xyz>
2022-06-15 16:21:05 -0400

    Prepare Cargo publish

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tropical Algebra
+
+This package is an implementation of the tropical algebra in Rust.
+
+**Warning**: This package is in very early stages of development. Many features have not been implemented yet.
+
+## Planned Features:
+
+- [X] Implement the basic elements of the ring, and their associated operations.
+- [ ] Convert implementation to generics (to generalize from `f64` to any numeric type).
+- [ ] Add support for matrices (using tropical matrix addition and matrix multiplication).
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ struct MaxTropical(f64);
 
 impl MaxTropical {
     fn add(x: MaxTropical, y: MaxTropical) -> MaxTropical {
-       MaxTropical(x.x.max(y.x))
+        MaxTropical(x.0.max(y.0))
     }
 
     fn multiply(x: MaxTropical, y: MaxTropical) -> MaxTropical {
-       MaxTropical(x.x + y.x)
+        MaxTropical(x.0 + y.0)
     }
 
     fn ninf() -> MaxTropical {
@@ -15,36 +15,80 @@ impl MaxTropical {
     }
 }
 
+#[derive(PartialEq, PartialOrd, Debug)]
+struct MinTropical(f64);
+
+impl MinTropical {
+    fn add(x: MinTropical, y: MinTropical) -> MinTropical {
+        MinTropical(x.0.min(y.0))
+    }
+
+    fn multiply(x: MinTropical, y: MinTropical) -> MinTropical {
+        MinTropical(x.0 + y.0)
+    }
+
+    fn inf() -> MinTropical {
+        MinTropical(f64::INFINITY)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::MaxTropical;
+    use crate::{MaxTropical, MinTropical};
 
     #[test]
-    fn addition() {
+    fn max_addition() {
         let x = MaxTropical(5.0);
         let y = MaxTropical(1.0);
         assert_eq!(MaxTropical::add(x, y), MaxTropical(5.0));
     }
 
     #[test]
-    fn multiplication() {
+    fn max_multiplication() {
         let x = MaxTropical(5.0);
         let y = MaxTropical(1.0);
         assert_eq!(MaxTropical::multiply(x, y), MaxTropical(6.0));
     }
 
     #[test]
-    fn identity_addition() {
+    fn max_identity_addition() {
         let ninf = MaxTropical::ninf();
         let x = MaxTropical(5.0);
         assert_eq!(MaxTropical::add(x, ninf), MaxTropical(5.0));
     }
 
     #[test]
-    fn identity_multiplication() {
+    fn max_identity_multiplication() {
         let zero = MaxTropical(0.0);
         let x = MaxTropical(5.0);
         assert_eq!(MaxTropical::multiply(x, zero), MaxTropical(5.0));
     }
 
+    #[test]
+    fn min_addition() {
+        let x = MinTropical(5.0);
+        let y = MinTropical(1.0);
+        assert_eq!(MinTropical::add(x, y), MinTropical(1.0));
+    }
+
+    #[test]
+    fn min_multiplication() {
+        let x = MinTropical(5.0);
+        let y = MinTropical(1.0);
+        assert_eq!(MinTropical::multiply(x, y), MinTropical(6.0));
+    }
+
+    #[test]
+    fn min_identity_addition() {
+        let ninf = MinTropical::inf();
+        let x = MinTropical(5.0);
+        assert_eq!(MinTropical::add(x, ninf), MinTropical(5.0));
+    }
+
+    #[test]
+    fn min_identity_multiplication() {
+        let zero = MinTropical(0.0);
+        let x = MinTropical(5.0);
+        assert_eq!(MinTropical::multiply(x, zero), MinTropical(5.0));
+    }
 }
